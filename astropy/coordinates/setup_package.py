040729657ee95d039f4411ff1956fbf53cefc24d
Erik M. Bray <embray@stsci.edu>
2013-05-19 16:21:12 -0400

    Saving a WIP while I go to deal with some bad docstrings I just noticed.

--- astropy/coordinates/setup_package.py
+++ astropy/coordinates/setup_package.py
@@ -1,3 +1,11 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+import sys
+
+if sys.version_info[0] < 3:
+    from astropy.coordinates.angle_utilities import _AngleParser
+
+    _AngleParser()
+
 def get_package_data():
     return {'astropy.coordinates.tests.accuracy': ['*.csv']}
